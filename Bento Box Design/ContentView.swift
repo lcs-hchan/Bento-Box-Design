d0270427f7bc81a7b014992526032082796b13c3
Hayden Chan <hchan@lcs.on.ca>
2023-11-01 19:38:51 -0400

    almost finish with the basic pictures and structures

--- Bento Box Design/ContentView.swift
+++ Bento Box Design/ContentView.swift
@@ -81,6 +81,7 @@ struct ContentView: View {
                                 Spacer()
                                 Text("20%")
                                     .font(.system(size:30))
+                                    .foregroundColor(.purple)
                                 Spacer()
                                 Text("faster CPU")
                                     .foregroundStyle(.black)
@@ -96,8 +97,12 @@ struct ContentView: View {
                             .frame(height: 125)
                                 VStack {
                                     Text("Over")
+                                        .foregroundColor(.purple)
                                     Text("67 billion")
+                                        .font(.system(size: 40))
+                                        .foregroundColor(.purple)
                                     Text("transistors")
+                                        .foregroundColor(.purple)
                                 }
                             
                             
@@ -112,10 +117,14 @@ struct ContentView: View {
                                 .frame(height:100)
                             VStack {
                                 Text("16-core")
-                                Text("Neural")
-                                Text("Engine")
+                                    .foregroundColor(.black)
+                                Text("Neural Engine")
+                                    .foregroundColor(.purple)
+                                    .font(.system(size: 25))
                                 Text("15.8 trillion people")
+                                    .foregroundColor(.black)
                             }
+                            
                         }
                         
                         ZStack {
@@ -123,9 +132,14 @@ struct ContentView: View {
                                 .fill(mediumGray)
                             .frame(height:100)
                             VStack{
-                            
+                                
                                 Text("40%")
+                                    .foregroundColor(.purple)
+                                    .font(.system(size: 30))
+                                    
+                                
                                 Text("Faster Neural Engine")
+                                    .foregroundColor(.black)
                                 
                             }
                             
@@ -138,8 +152,10 @@ struct ContentView: View {
                         VStack {
                             Text("High-Performance")
                                 .foregroundStyle(.black)
+                                .font(.system(size: 17))
                             Text("media engine with ProRes")
                                 .foregroundStyle(.black)
+                                .font(.system(size: 17))
                         }
                         
                     }
@@ -215,7 +231,11 @@ struct ContentView: View {
                             .frame(height: 75)
                         VStack{
                             Text("Second generation")
-                            Text("5nm technology")
+                                .foregroundColor(.black)
+                                .font(.system(size: 10))
+                            Text("5 nm technology")
+                                .foregroundColor(.purple)
+                                .font(.system(size: 30))
                         }
                         
                     }
@@ -253,6 +273,13 @@ struct ContentView: View {
                                         .scaledToFit()
                                     .frame(height: 100)
                                     .padding()
+                                    Text("12-core")
+                                        .foregroundColor(.purple)
+                                        .font(.system(size: 20))
+                                    
+                                    Text("CPU")
+                                        .foregroundColor(.black)
+                                        .font(.system(size: 10))
                                     
                                    
                                 }
@@ -268,9 +295,21 @@ struct ContentView: View {
                             
                         
                     }
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(mediumGray)
-                        .frame(height: 120)
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(mediumGray)
+                            .frame(height: 120)
+                        VStack{
+                            Text("400GB/s")
+                                .foregroundColor(.purple)
+                                .font(.system(size: 40))
+                            Text("Memory bandwidth")
+                                .foregroundColor(.black)
+                                .font(.system(size: 15))
+                        }
+                    }
+
                 }
             }
         }
